52da724c998c88f6eb8e14e5ad711f849d2d30df
David McCartney <dmccartney@coreslab.com>
2017-07-19 14:17:10 -0500

    Initial Commit

---
+++ source/Core/TWXC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TWXP;
+
+namespace TWXC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TScript ts = new TScript();
+            ts.Save(@"c:\tw2002\test\test1.cts", true);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
