91641a2473623ec55cd9a9c0f47b568888e32799
David McCartney <dmccartney@coreslab.com>
2017-09-08 15:55:58 -0500

    Initial Commit

---
+++ Source/Shared/Terminal/Display.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terminal
+{
+    public class Display
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public int[,] Data { get; private set; }
+
+        public Display()
+        {
+            Height = 24;
+            Width = 80;
+
+            Data = new int[Height, Width];
+        }
+
+        public Display(int width, int height)
+        {
+            Height = width;
+            Width = height;
+
+            Data = new int[Height, Width];
+        }
+
+        public void Resize(int width, int height)
+        {
+            Height = width;
+            Width = height;
+
+            Data = new int[Height, Width];
+        }
+
+    }
+}
